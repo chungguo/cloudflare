5dff81183faadd47298dc1b77a6ce13cf7ae4673
chungguo <chungguo.gc@gmail.com>
2023-10-05 15:47:29 +0800

    feat: wait for cloudflare browser rendering

--- src/index.ts
+++ src/index.ts
@@ -5,17 +5,18 @@ import ptSitesSignIn from './jobs/pt';
 
 export default {
 	async scheduled(event: ScheduledEvent, env: Env): Promise<void> {
-		switch (event.cron) {
-			// PT 签到
-			case "0 10 * * *":
-				await ptSitesSignIn(env);
-				break;
-			// JD 价格保护
-			case "0 12 * * *":
-				// await priceProtect(env);
-				break;
-			default:
-				return;
-		}
+		await ptSitesSignIn(env);
+		// switch (event.cron) {
+		// 	// PT 签到
+		// 	case "0 10 * * *":
+		// 		await ptSitesSignIn(env);
+		// 		break;
+		// 	// JD 价格保护
+		// 	case "0 12 * * *":
+		// 		await priceProtect(env);
+		// 		break;
+		// 	default:
+		// 		return;
+		// }
 	},
 };
